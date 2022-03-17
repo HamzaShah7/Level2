932a97f02537010f5cf5907ad84d821578a64ec6
HamzaShah7 <56911408+HamzaShah7@users.noreply.github.com>
2022-03-17 18:12:29 +0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.lang.Math;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 
 public class Employee {
@@ -60,93 +60,40 @@ public class Employee {
     }
 
     public void AddSal(){
-        double Sal = getSalary ();
-        if (Sal<500){
-            setSalary (Sal+10);
+        if (getSalary ()<500){
+            setSalary (getSalary ()+10);
+        }
+        else {
+            System.out.println ("Your Salary is greater than $500.");
         }
     }
 
     public void AddWork(){
-        double hour = getHours ();
-        if (hour>6){
+        if (getHours ()>6){
             setSalary (getSalary ()+5);
         }
-    }
-
-    public static void main (String[] args) {
-
-        ArrayList<Employee> employees = new ArrayList<> ();
-        Scanner sc = new Scanner (System.in);
-        int letter = 5;
-        //int Emp_No = (int) (Math.random()*(999-100+1)+100);
-        int Emp_No = 100;
-        while(letter!=0){
-            if (employees.isEmpty ()){
-                System.out.println ("Enter Employee Details");
-                letter = 1;
-            }
-            else {
-                System.out.print ("Enter 0 to Exit\nEnter 1 to Add New Employee\nEnter 2 to Add $10 to Salary \nEnter 3 to Add $5 to Salary\nEnter 4 to Show Employee Details. ");
-                letter = sc.nextInt ();
-            }
-            switch (letter){
-                case 1:
-                    System.out.print ("Enter Your Name: ");
-                    String Name = sc.next ();
-                    System.out.print ("Enter Your Salary: ");
-                    double Salary = sc.nextDouble ();
-                    System.out.print ("Enter Your Hours: ");
-                    float Hours = sc.nextFloat ();
-                    Employee employee = new Employee ();
-                    Emp_No = Emp_No + 1;
-                    employees.add (employee.getInfo (Emp_No,Name,Salary,Hours));
-                    break;
-
-                case 2:
-                    if (!(employees.isEmpty ())){
-                        for (Employee p : employees){
-                            System.out.println (p);
-                        }
-                        System.out.print ("Enter Your ID: ");
-                        int N = sc.nextInt ();
-                        for (Employee p : employees){
-                            if (N==p.ID){
-                                p.AddSal ();
-                                System.out.println (p);
-                            }
-                        }
-                    }
-                    else {
-                        System.out.println ("Your list is empty");
-                    }
-                    break;
-
-                case 3:
-                    if (!(employees.isEmpty ())){
-                        System.out.print ("Enter Your ID: ");
-                        int S = sc.nextInt ();
-                    for (Employee p : employees){
-                            if (S==p.ID){
-                                p.AddWork ();
-                                System.out.println (p);
-                            }
-                        }
-                    }
-                    else {
-                        System.out.println ("Your List is empty.");
-                    }
-                case 4:
-                    System.out.print ("Enter Your ID: ");
-                    int S = sc.nextInt ();
-                    for (Employee p : employees){
-                        if (S==p.ID){
-                            System.out.println (p);
-                        }
-                    }
-                    break;
-            }
+        else {
+            System.out.println ("Your worked less than 6 hours.");
         }
     }
+
+    public static Employee search(ArrayList<Employee> employees, String search)
+    {
+        for (Employee employee: employees) {
+            if(Double.toString(employee.getSalary()).equals (search) || employee.getName ().equals (search))
+                return employee;
+        }
+    return null;
+    }
+
+
+    public static Optional<Employee> searchPredicate(ArrayList<Employee> employees,String search)
+    {
+
+        Predicate<Employee> isName = employee -> employee.getName ().equalsIgnoreCase (search);
+        Predicate<Employee> isSalary = employee -> Double.toString(employee.getSalary ()).equalsIgnoreCase (search);
+        return employees.stream().filter (isName.or(isSalary)).findFirst();
+
+    }
+
 }
-
-
